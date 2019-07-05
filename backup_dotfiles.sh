786919277f3332c3f077e8db9ae9a080b0426fd5
Rafael Dias <rafael.silveira@poupex.com.br>
2019-07-05 09:29:29 -0300

    feat(script): create backup function

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -2,34 +2,38 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-
 ERROR_MSG="[${RED}ERROR${NC}]"
 SUCCESS_MSG="[${GREEN}SUCCESS${NC}]"
 
-# Input do ambiente (home ou work)
-ambiente=$1
-echo "Backup do ambiente $ambiente"
+backup() {
+	ambiente=$1
+	echo "Backup do ambiente $ambiente"
 
-git checkout $ambiente
+	git checkout $ambiente
 
-# git
-cp ~/.gitmessage .
-cp ~/.gitconfig .
+	# git
+	cp ~/.gitmessage .
+	cp ~/.gitconfig .
 
-# nvim
-rsync -r -aqz --exclude 'plugged' --links ~/.config/nvim/ .config/nvim/
+	# nvim
+	rsync -r -aqz --exclude 'plugged' --links ~/.config/nvim/ .config/nvim/
 
-# vim
-rsync -r --exclude 'bundle' --links ~/.vim/ .vim/
+	# vim
+	rsync -r --exclude 'bundle' --links ~/.vim/ .vim/
 
-# tmux
-cp ~/.tmux.conf .
+	# tmux
+	cp ~/.tmux.conf .
 
-# tmuxinator
-rsync -r --links ~/.config/tmuxinator .config/tmuxinator
+	# tmuxinator
+	rsync -r --links ~/.config/tmuxinator .config/tmuxinator
 
-# shell (zshrc and bashrc)
-cp ~/.zshrc ~/.zsh_profile ~/.bashrc ~/.bash_profile ~/.purepower .
+	# shell (zshrc and bashrc)
+	cp ~/.zshrc ~/.zsh_profile ~/.bashrc ~/.bash_profile ~/.purepower .
 
-# Lein (clojure)
-rsync -r --links ~/.lein .
+	# Lein (clojure)
+	rsync -r --links ~/.lein .
+}
+
+restore() {
+
+}
