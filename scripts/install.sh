ee67bf44ebd338f7f63d571112f74513a9cb0d2f
Rafael Dias <rafael.silveira@poupex.com.br>
2019-07-23 12:16:29 -0300

    feat(#5): docker setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,8 +21,40 @@ else
 fi
 
 #################### Nodejs ####################
-if [ $(node -v) ]; then
+if [[ $(node -v) ]]; then
 	echo "Node.js já instalado"
 else
 	snap install node --classic --channel=12
+fi
+
+#################### Docker ####################
+if [[ $(docker -v) ]]; then
+	echo "Docker já instalado"
+else
+	# Install deps
+	apt-get install \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg2 \
+		software-properties-common
+
+	# Add GPG key
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+	# Add repo
+	add-apt-repository \
+		"deb [arch=amd64] https://download.docker.com/linux/debian \
+		$(lsb_release -cs) \
+		stable"
+
+	apt update
+
+	# Install docker
+	apt-get install docker-ce docker-ce-cli containerd.io
+
+	# Post install
+	groupadd docker
+	usermod -aG docker $USER
+	newgrp docker 
 fi
