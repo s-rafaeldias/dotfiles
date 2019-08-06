d9524e9dd02cc7faae8a88f715226c90c8594eac
Rafael Dias <rafael.silveira@poupex.com.br>
2019-08-06 13:25:31 -0300

    feat: add _helper.sh

---
+++ scripts/_helpers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+echo_error() {
+	printf '\n\033[31mERROR:\033[0m %s\n' "$1"
+}
+
+echo_warning() {
+  printf '\n\033[33mWARNING:\033[0m %s\n' "$1"
+}
+
+echo_done() {
+  printf '\n\033[32mDONE:\033[0m %s\n' "$1"
+}
+
+echo_info() {
+  printf '\n\033[36m%s\033[0m\n' "$1"
+}
+
+echo_error "teste"
+echo_warning "teste"
+echo_done "teste"
+echo_info "teste"
