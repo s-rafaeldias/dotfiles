1530e936f6c4131490c55507eb6afdf163f076a0
Rafael Dias <rafael.silveira@poupex.com.br>
2020-01-02 14:31:14 -0300

    refactor(emacs)

--- emacs/init.el
+++ emacs/init.el
@@ -27,26 +27,3 @@ There are two things you can do about this warning:
 (use-package org :ensure org-plus-contrib)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-;; Auto generated code
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(conda-env-home-directory "~/miniconda3")
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages
-   (quote
-    (projectile haskell-mode rainbow-delimiters highlight-parentheses spacemacs-dark spacemacs-theme conda company-anaconda anaconda-mode company-jedi flycheck counsel consuel company evil company-mode wakatime-mode swiper ace-window helm which-key use-package)))
- '(wakatime-cli-path "~/miniconda3/bin/wakatime")
- '(wakatime-python-bin nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
