91d1c0115c642f696eecbbf2ad9439dd772668c3
Rafael Dias <rafa197795@gmail.com>
2020-08-15 19:47:20 -0300

    update

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install neovim from source
+git clone git@github.com:neovim/neovim.git ~/tmp/neovim
+cd ~/tmp/neovim
+make
+sudo make install
+cd -
