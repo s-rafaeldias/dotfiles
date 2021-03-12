233751bffa1868b0d371b4ce7528705c8f74a647
Rafael Dias <rafa197795@gmail.com>
2021-03-12 14:03:17 -0300

    feat(nvim): remap <C-P> for telescope.find_files() only

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,15 +4,8 @@ local telescope  = require 'telescope.builtin'
 require 'rds.lsp'
 require 'rds.snippets'
 
--- switch between `find_files` and `git_files` using the same mapping
-vimp.nnoremap('<C-P>', function()
-    -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/builtin/git.lua#L141
-    local cwd = vim.loop.cwd()
-    local _ = vim.fn.systemlist("git -C "..cwd.." rev-parse --show-toplevel")[1]
+-- MAPPINGS
+vimp.nnoremap('<C-P>', function() telescope.find_files() end)
 
-    if vim.v.shell_error ~= 0 then
-        telescope.find_files{}
-    else
-        telescope.git_files{}
-    end
-end)
+
+-- Reload configs
