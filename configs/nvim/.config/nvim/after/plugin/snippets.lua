a3124c7952620c5a011f09b02d6efed9109c1515
Rafael Dias <s.rafaeldias@protonmail.com>
2023-04-16 17:39:46 -0300

    feat(nvim-py): add new snippets for python

--- configs/nvim/.config/nvim/after/plugin/snippets.lua
+++ configs/nvim/.config/nvim/after/plugin/snippets.lua
@@ -1,15 +1,15 @@
 local ls = require "luasnip"
 local s = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require "luasnip.util.events"
-local ai = require "luasnip.nodes.absolute_indexer"
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require "luasnip.util.events"
+-- local ai = require "luasnip.nodes.absolute_indexer"
 local fmt = require("luasnip.extras.fmt").fmt
 
 -- Snippets
@@ -58,7 +58,7 @@ ls.add_snippets("python", {
   pytest,
   todo,
   main_py,
-  lambda_py
+  lambda_py,
 })
 
 ls.add_snippets("sh", {
