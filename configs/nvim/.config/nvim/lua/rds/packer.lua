2e44e48159b6b214a2749b652acaf3fa5245bf90
Rafael Dias <s.rafaeldias@protonmail.com>
2023-06-30 22:03:31 -0300

    refactor(nvim): plugin's cleanup on packer

--- configs/nvim/.config/nvim/lua/rds/packer.lua
+++ configs/nvim/.config/nvim/lua/rds/packer.lua
@@ -1,31 +1,91 @@
 require("packer").startup(function(use)
-  -- Utils
+  -- Essentials {{{
   use "wbthomason/packer.nvim"
-  use "nvim-lua/plenary.nvim"
-  use "nvim-lua/popup.nvim"
-  use "kyazdani42/nvim-web-devicons"
   use "lewis6991/impatient.nvim"
-
   use "tpope/vim-projectionist"
   use "tpope/vim-speeddating"
   -- Super duper plugin to help surrounding text-objects
   use "tpope/vim-surround"
   -- Show macros and registers
   use "junegunn/vim-peekaboo"
+  use "ThePrimeagen/harpoon"
 
   use {
-    "vimwiki/vimwiki",
-    branch = "dev",
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
   }
-  -- use "mattdibi/incolla.nvim"
-  -- use "edluffy/hologram.nvim"
 
-  use "ThePrimeagen/harpoon"
-  -- use "mbbill/undotree"
+  use "tpope/vim-fugitive"
 
-  -- Theme
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use { "nvim-treesitter/playground", run = ":TSInstall query" }
+  use "nvim-treesitter/nvim-treesitter-textobjects"
+  use "nvim-treesitter/nvim-treesitter-context"
+  use "nvim-treesitter/nvim-treesitter-refactor"
+
+  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use "nvim-lua/telescope.nvim"
+  use "nvim-telescope/telescope-ui-select.nvim"
+  -- }}}
+
+  -- Good to have {{{
+  use "ThePrimeagen/git-worktree.nvim"
+  use {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup {}
+    end,
+  }
+  use "williamboman/mason-lspconfig.nvim"
+  use "WhoIsSethDaniel/mason-tool-installer.nvim"
+
+  use { "L3MON4D3/LuaSnip", rtp = "." }
+  -- use "rafamadriz/friendly-snippets"
+  -- Can I make it work well with omnifunc?
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      -- "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
+      -- "petertriho/cmp-git",
+    },
+  }
+
+  -- LSP stuff {{{2
+  -- use "folke/trouble.nvim"
+  use "neovim/nvim-lspconfig"
+  use "nvim-lua/lsp_extensions.nvim"
+  use "nvim-lua/lsp-status.nvim"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "ray-x/lsp_signature.nvim"
+  -- }}}
+
+  -- }}}
+
+  -- Language specifics {{{
+  use "NoahTheDuke/vim-just"
+  use "fatih/vim-go"
+  use "simrat39/rust-tools.nvim"
+  use "scalameta/nvim-metals"
+
+  -- Debugging {{{2
+  use "mfussenegger/nvim-dap"
+  use "rcarriga/nvim-dap-ui"
+  use "leoluz/nvim-dap-go"
+  use "mfussenegger/nvim-dap-python"
+  -- }}}
+  -- }}}
+
+  -- Theme {{{
   -- use "gruvbox-community/gruvbox"
   -- use "joshdick/onedark.vim"
+  use "nvim-lualine/lualine.nvim"
   use {
     "catppuccin/nvim",
     as = "catppuccin",
@@ -42,105 +102,11 @@ require("packer").startup(function(use)
       }
     end,
   }
-
-  -- statusline
-  use "nvim-lualine/lualine.nvim"
-
-  use {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  }
-
-  -- Git stuff {{{
-  use {
-    "tpope/vim-fugitive",
-    requires = {
-      -- GBrowse for GitHub
-      -- "tpope/vim-rhubarb",
-      -- GBrowse for GitLab
-      -- "shumphrey/fugitive-gitlab.vim",
-    },
-  }
-  -- use "TimUntersberger/neogit"
-  use "ThePrimeagen/git-worktree.nvim"
-  -- use {
-  --   "akinsho/git-conflict.nvim",
-  --   tag = "*",
-  -- }
   -- }}}
 
-  -- Debugging {{{
-  use "mfussenegger/nvim-dap"
-  use "rcarriga/nvim-dap-ui"
-  -- use "leoluz/nvim-dap-go"
-  use "mfussenegger/nvim-dap-python"
+  -- Utils {{{
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/popup.nvim"
+  use "kyazdani42/nvim-web-devicons"
   -- }}}
-
-  -- LSP stuff {{{
-  -- use "folke/trouble.nvim"
-  use "neovim/nvim-lspconfig"
-  use "nvim-lua/lsp_extensions.nvim"
-  use "nvim-lua/lsp-status.nvim"
-  use "jose-elias-alvarez/null-ls.nvim"
-  -- use "j-hui/fidget.nvim"
-  use "ray-x/lsp_signature.nvim"
-  use "simrat39/rust-tools.nvim"
-  use "scalameta/nvim-metals"
-  -- }}}
-
-  -- Auto complete stuff {{{
-  use {
-    "hrsh7th/nvim-cmp",
-    requires = {
-      -- "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      -- "petertriho/cmp-git",
-    },
-  }
-  -- snippets
-  use { "L3MON4D3/LuaSnip", rtp = "." }
-  -- use "rafamadriz/friendly-snippets"
-  -- }}}
-
-  -- Treesitter {{{
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-  use { "nvim-treesitter/playground", run = ":TSInstall query" }
-  use "nvim-treesitter/nvim-treesitter-textobjects"
-  use "nvim-treesitter/nvim-treesitter-context"
-  use "nvim-treesitter/nvim-treesitter-refactor"
-  use "NoahTheDuke/vim-just"
-
-  -- use "Afourcat/treesitter-terraform-doc.nvim"
-  -- }}}
-
-  -- Telescope {{{
-  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
-  use "nvim-lua/telescope.nvim"
-  use "nvim-telescope/telescope-ui-select.nvim"
-  -- }}}
-
-  -- SQL stuff {{{
-  -- use {
-  --   "tpope/vim-dadbod",
-  --   requires = {
-  --     "kristijanhusak/vim-dadbod-completion",
-  --     "kristijanhusak/vim-dadbod-ui",
-  --   },
-  -- }
-  -- }}}
-
-  use {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup {}
-    end,
-  }
-  use "williamboman/mason-lspconfig.nvim"
-  use "WhoIsSethDaniel/mason-tool-installer.nvim"
 end)
