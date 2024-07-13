1a4aa4924e45c3cdf3c76f549f4bfd5302777f65
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2024-07-13 11:02:43 +0200

    feat(nvim-lsp): remove scala lsp code

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -729,25 +729,6 @@ vim.g.rustaceanvim = {
 -- }
 -- }}}
 
--- Metals (scala) {{{
--- local metals_config = require("metals").bare_config()
--- metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
--- metals_config.on_attach = custom_attach
--- metals_config.init_options.statusBarProvider = "on"
---
--- local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
--- vim.api.nvim_create_autocmd("FileType", {
---   -- NOTE: You may or may not want java included here. You will need it if you
---   -- want basic Java support but it may also conflict if you are using
---   -- something like nvim-jdtls which also works on a java filetype autocmd.
---   pattern = { "scala", "sbt" },
---   callback = function()
---     require("metals").initialize_or_attach(metals_config)
---   end,
---   group = nvim_metals_group,
--- })
--- }}}
-
 -- Null-ls {{{
 null_ls.setup {
   sources = {
