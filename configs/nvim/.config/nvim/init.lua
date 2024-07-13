685c49713c4df954cdc086b4aad87a4434cdd44f
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2024-07-13 11:02:11 +0200

    feat(nvim-lsp): remove arduino lsp code

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -171,7 +171,7 @@ require("packer").startup(function(use)
     end,
   }
   -- use "scalameta/nvim-metals"
-  -- use "mfussenegger/nvim-jdtls"
+  use "mfussenegger/nvim-jdtls"
   use { "elixir-tools/elixir-tools.nvim", tag = "stable" }
   use {
     "mattn/emmet-vim",
@@ -700,23 +700,6 @@ lspconfig["lua_ls"].setup {
     },
   },
 }
--- }}}
-
--- Arduino {{{
--- lspconfig["arduino_language_server"].setup {
---   on_attach = custom_attach,
---   cmd = {
---     "arduino-language-server",
---     "-cli-config",
---     "/Users/rafael/Library/Arduino15/arduino-cli.yaml",
---     "-fqbn",
---     "esp8266:esp8266:nodemcu",
---     "-cli",
---     "arduino-cli",
---     "-clangd",
---     "clangd",
---   },
--- }
 -- }}}
 
 -- Markdown {{{
