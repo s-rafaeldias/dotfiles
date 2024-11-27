ba81ffc0ed7d12bd8cbe87c33ec096ec5217c4bc
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2024-11-27 19:39:11 +0100

    chore(nvim): remove unused files

--- configs/.config/nvim/lua/rds/init.lua
+++ configs/.config/nvim/lua/rds/init.lua
@@ -1,8 +1,1 @@
-require "rds.plugins.pydoc"
-require "rds.plugins.notes"
-require "rds.plugins.lsp"
-
-function R()
-  require("rds.utils").reload "rds.plugins.pydoc"
-  require("rds.plugins.pydoc").run()
-end
+-- require "rds.plugins.lsp"
